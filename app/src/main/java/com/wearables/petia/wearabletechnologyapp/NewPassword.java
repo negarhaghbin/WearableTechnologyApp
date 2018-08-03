b864fa0b7475c398be13ca598fba96dc9dd4de94
PetiaPav <petia2397@yahoo.ca>
2018-08-02 20:17:30 -0400

    Initial commit

---
+++ app/src/main/java/com/wearables/petia/wearabletechnologyapp/NewPassword.java
@@ -0,0 +1,13 @@
+package com.wearables.petia.wearabletechnologyapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_password);
+    }
+}
