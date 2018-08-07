e96bbc669c1e37860d3a974c88684d196216288d
K-Langla <kalanglais@gmail.com>
2018-08-07 10:16:42 -0400

    settings initialization

---
+++ app/src/main/java/com/wearables/petia/wearabletechnologyapp/SettingsActivity.java
@@ -0,0 +1,13 @@
+package com.wearables.petia.wearabletechnologyapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_main_page);
+    }
+}
