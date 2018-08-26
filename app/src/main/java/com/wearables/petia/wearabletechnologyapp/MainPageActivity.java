1420b61694287dc5db989bd5727855a731605334
K_Langlais <kalanglais@gmail.com>
2018-08-26 17:56:42 -0400

    Settings page modifications

--- app/src/main/java/com/wearables/petia/wearabletechnologyapp/MainPageActivity.java
+++ app/src/main/java/com/wearables/petia/wearabletechnologyapp/MainPageActivity.java
@@ -78,14 +78,17 @@ public class MainPageActivity extends AppCompatActivity
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        switch (item.getItemId()) {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+
+            case R.id.action_settings:
+                Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
