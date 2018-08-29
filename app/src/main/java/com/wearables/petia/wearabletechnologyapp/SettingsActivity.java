e914ffa7f1b81e5ee034811272cd858c6c47dabb
K_Langlais <kalanglais@gmail.com>
2018-08-28 22:59:41 -0400

    added AppCompatPreferenceActivity.java, fixed appcompatpreferenceactivity not resolving

--- app/src/main/java/com/wearables/petia/wearabletechnologyapp/SettingsActivity.java
+++ app/src/main/java/com/wearables/petia/wearabletechnologyapp/SettingsActivity.java
@@ -3,7 +3,7 @@ package com.wearables.petia.wearabletechnologyapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatPreferenceActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
