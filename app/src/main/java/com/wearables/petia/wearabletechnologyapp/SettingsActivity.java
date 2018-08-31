0aa5fef58794caa45e0c9ec427638fc09f830d81
K_Langlais <kalanglais@gmail.com>
2018-08-31 16:45:00 -0400

    trying to get preferenceFragments compat library for Fragment.replace

--- app/src/main/java/com/wearables/petia/wearabletechnologyapp/SettingsActivity.java
+++ app/src/main/java/com/wearables/petia/wearabletechnologyapp/SettingsActivity.java
@@ -1,13 +1,24 @@
 package com.wearables.petia.wearabletechnologyapp;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SettingsActivity extends AppCompatPreferenceActivity {
+public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_main_page);
+        // Begin the transaction
+        PreferenceFragment settingsFragment = new PreferenceFragment();
+        settingsFragment.addPreferencesFromResource(R.xml.preferences);
+
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        // Replace the contents of the container with the new fragment
+        ft.replace(R.id.sensor_fragment_container, settingsFragment );
+        // or ft.add(R.id.your_placeholder, new FooFragment());
+        // Complete the changes added above
+        ft.commit();
     }
 }
